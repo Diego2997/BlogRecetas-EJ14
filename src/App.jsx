8f20cb6ef79f53edc6137fc14af3cdd7075ef366
Emanuel Ruiz <ruizemanuelok@gmail.com>
2023-06-17 19:55:23 -0300

    creacion de formulario crear receta

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import './App.css'
 import Admin from './components/pages/Admin';
+import CrearReceta from './components/pages/Recetas/CrearReceta';
 
 function App() {
 
   return (
     <>
-    <Admin></Admin>
+    {/* <Admin></Admin> */}
+    <CrearReceta></CrearReceta>
     </>
   )
 }
