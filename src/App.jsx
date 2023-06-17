af9a179d43c7f5d434338ced3bca2d542b848a49
RLauraS <R_LauraS@yahoo.com.ar>
2023-06-16 23:39:36 -0300

    Se inicia con el maquetado y parte de la logica del login siguiendo lo visto en clase

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css'
+import Registro from './components/pages/Registo';
 
 function App() {
 
   return (
     <>
+    <Registro></Registro>
     </>
   )
 }
