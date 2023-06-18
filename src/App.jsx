e671e118f0d39aff516ffddc70301b86f6ce14da
Diego Mercado <diego.mercado1997@hotmail.com>
2023-06-18 10:53:42 -0300

    se agrega el footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css'
+import Footer from './components/common/Footer';
 
 function App() {
 
   return (
     <>
+    <Footer/>
     </>
   )
 }
