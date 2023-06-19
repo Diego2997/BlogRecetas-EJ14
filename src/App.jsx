b3b4ae54b98587144f74e7344b99a0a0bfbd1acf
Diego Mercado <diego.mercado1997@hotmail.com>
2023-06-19 19:57:35 -0300

    agregando ruteo en app

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css'
 import { BrowserRouter,Route,Routes} from 'react-router-dom';
 import Footer from './components/common/Footer'
+import Menu from './components/common/Menu'
 
 function App() {
 
   return (
     <>
     <BrowserRouter>
+    {/* <Menu></Menu> */}
       <Routes>
       {/* AQUI VAN LAS RUTAS */}
       </Routes>
