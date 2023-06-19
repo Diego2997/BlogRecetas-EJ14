7c03b6cd653ea30acf7de010266985af19a3b2ca
Diego Mercado <diego.mercado1997@hotmail.com>
2023-06-19 10:40:53 -0300

    ruteo en app

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css'
+import { BrowserRouter,Route,Routes} from 'react-router-dom';
+import Footer from './components/common/Footer'
 
 function App() {
 
   return (
     <>
+    <BrowserRouter>
+      <Routes>
+      {/* AQUI VAN LAS RUTAS */}
+      </Routes>
+      <Footer/>
+    </BrowserRouter>
     </>
   )
 }
